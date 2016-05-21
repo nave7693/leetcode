4a26b4eb91486a661cc836c9e51b747fbe3ad141
nave7693 <nave7693@digitcastle.net>
2016-05-21 00:07:19 -0700

    Today's solution

---
+++ 011-container-with-most-water.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int result = 0;
+        while (left < right) {
+            result = max(result, min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return result;
+    }
+};
