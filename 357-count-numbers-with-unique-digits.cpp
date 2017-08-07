16224c3f25ea616664119a2aba8baf81983590a7
nave7693 <nave7693@nave7693.com>
2017-08-06 22:30:49 -0700

    Problems for 8/4

--- 357-count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits.cpp
@@ -25,3 +25,24 @@ public:
         return result;
     }
 };
+
+// 8/3
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        
+        int prev = 9; // number of unique digits that can start a number of length 1
+        int sum = 10;
+        int curr;
+        for (int i = 2; i <= min(10, n); i++) {
+            // for each of the numbers in prev, we can pick a new unique digit and attach to the end
+            // in the case of the second digit, we can still choose 9 because the first digit can only
+            // be one of nine choices (no zero).
+            curr = prev * (10-i+1);
+            sum += curr;
+            prev = curr;
+        }
+        return sum;
+    }
+};
